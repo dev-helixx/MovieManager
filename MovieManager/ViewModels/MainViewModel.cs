35589e7c2c9c847d0a730de8ddead58c3fa21909
Silas <ses@cBrainAS.onmicrosoft.com>
2019-02-21 09:26:10 +0100

    Implemented Save values functionality

--- MovieManager/ViewModels/MainViewModel.cs
+++ MovieManager/ViewModels/MainViewModel.cs
@@ -1,22 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Xml.Serialization;
 using MovieManager.Command.MovieManager.Command;
 using MovieManager.Models;
 using MovieManager.ViewModels;
 
 namespace MovieManager.ViewModel
 {
-  public class MainWindowViewModel
+  public class MainWindowViewModel : BaseViewModel
   {
+
+
+    #region Properties
 
     public MoviesViewModel MoviesViewModel { get; set; }
 
     public ActionCommand LoadCommand { get; set; }
     public ActionCommand SaveCommand { get; set; }
+
+
+
+    /* If any changes have been made to any of the UI fields */
+    private bool _changesDetected;
+    public bool ChangesDetected
+    {
+      get { return _changesDetected; }
+      set
+      {
+        if (value != _changesDetected)
+        {
+          _changesDetected = value;
+          OnPropertyChanged(nameof(ChangesDetected));
+        }
+      }
+    }
+
+    #endregion
 
 
 
@@ -28,14 +52,50 @@ namespace MovieManager.ViewModel
 
       // Pass list of movie objects to the MoviesViewModel
       MoviesViewModel = new MoviesViewModel(readingModel.Movies);
-
-      
-
-      // Do stuff with mainmodel object, which holds all information from db
+      // Add an even to check for changes in the viewmodel or if the ViewModels OnpropertyChanged event is called
+      MoviesViewModel.PropertyChanged += MoviesViewModel_PropertyChanged;
 
 
+      // Register commands so we are able to execute specific buttons
+      RegisterCommand(SaveCommand = new ActionCommand(Save, CanSave));
 
-   
+
+
+    }
+
+    private void MoviesViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+      ChangesDetected = true;
+    }
+
+    // Method for determining whether save button can be clicked or not
+    private bool CanSave()
+    {
+      return ChangesDetected;
+    }
+
+    // Method for handling save button logic
+    private void Save()
+    {
+
+      string DBPath = @"c:\tmp\movie_db.txt";
+      //* Xml Serilizer to write data to an existing txt file */
+      XmlSerializer x = new XmlSerializer(typeof(ReadingModel));
+      if (!string.IsNullOrWhiteSpace(DBPath) && File.Exists(DBPath))
+      {
+        using (TextWriter tw = new StreamWriter(DBPath))
+        {
+          // Update main model object with new values from textboxes
+          readingModel.Movies = MoviesViewModel.SaveValues();
+
+          x.Serialize(tw, readingModel);
+        }
+        MessageBox.Show("Values saved");
+      }
+
+      // Deactive Save button when values are saved
+      ChangesDetected = false;
+
     }
   }
 }
--- MovieManager/ViewModels/MoviesViewModel.cs
+++ MovieManager/ViewModels/MoviesViewModel.cs
@@ -4,11 +4,12 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using MovieManager.Models;
 
 namespace MovieManager.ViewModels
 {
-  public class MoviesViewModel
+  public class MoviesViewModel : BaseViewModel
   {
 
     #region Private Fields
@@ -56,14 +57,23 @@ namespace MovieManager.ViewModels
       foreach (MovieModel movie in movies)
       {
 
-        MoviesCollection.Add(new MovieViewModel(movie));
+        // For each movie object add a PropertyChanged event to it
+        MovieViewModel mvm = new MovieViewModel(movie);
+        mvm.PropertyChanged += Mvm_PropertyChanged;
+        MoviesCollection.Add(mvm);
 
-        //CustomerViewModel cvm = new CustomerViewModel(customer, this);
-        //// Attach PropertyChanged event to each object in the collection
-        //cvm.PropertyChanged += Cvm_PropertyChanged;
-        //Customers.Add(cvm);
+
+        //MoviesCollection.Add(new MovieViewModel(movie));
+
+        
 
       }
+    }
+
+    private void Mvm_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+      // Call OnPropertyChanged each time a change is detected in a movie object, which invokes the ViewModels OnPropertyChanged event defined in MainViewModel
+      OnPropertyChanged(nameof(Mvm_PropertyChanged));
     }
     #endregion
 
